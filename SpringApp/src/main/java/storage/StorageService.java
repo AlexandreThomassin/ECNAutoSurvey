c06509d2a765cad2c6add2569ba7c1963fa01f0d
chloe-caville <chloe.caville@gmail.com>
2024-02-23 19:17:55 +0100

    Add files to upload the syntheses

---
+++ SpringApp/src/main/java/storage/StorageService.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package storage;
+
+/**
+ *
+ * @author chloe
+ */
+
+import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
+public interface StorageService {
+
+	void init();
+
+	void store(MultipartFile file);
+
+	Stream<Path> loadAll();
+
+	Path load(String filename);
+
+	Resource loadAsResource(String filename);
+
+	void deleteAll();
+
+}
---
+++ SpringApp/src/main/java/uploadingfiles/UploadingFilesApplication.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package uploadingfiles;
+
+import org.springframework.context.annotation.Bean;
+import storage.StorageProperties;
+import storage.StorageService;
+
+/**
+ *
+ * @author chloe
+ */
+
+@SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
+public class UploadingFilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadingFilesApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
+	}
+}
